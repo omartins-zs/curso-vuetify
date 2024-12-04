88c21b740da71f70978283cb6cdbbcfd037a75d3
omartins-zs <gabrielmatheus209@gmail.com>
2024-12-04 10:02:09 -0300

    Usando Utilites(Border, Elevation, Spacing)

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,36 @@
       <v-app-bar-title>Meu APP</v-app-bar-title>
     </v-app-bar>
     <v-main>
-    <router-view />
+      <v-container>
+        <div class="d-flex justify-space-around">
+          <div
+            style="width: 100px; height: 100px"
+            class="border rounded-lg elevation-3"
+          >
+            teste
+          </div>
+
+          <div
+            style="width: 100px; height: 100px"
+            class="border rounded-lg elevation-3"
+          >
+            teste
+          </div>
+
+          <div
+            style="width: 100px; height: 100px"
+            class="border rounded-lg elevation-3"
+          >
+            teste
+          </div>
+        </div>
+      </v-container>
+
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script setup>
-  //
+//
 </script>
