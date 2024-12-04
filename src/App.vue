3ec2951c115e9bbb2a879b453ea87a0b86c80b8b
omartins-zs <gabrielmatheus209@gmail.com>
2024-12-04 10:38:39 -0300

    Conhecendo os Breakpoints

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,12 @@
     </v-app-bar>
     <v-main>
       <v-container>
+<h1 class="text-h5 text-lg-h3">Dashboard</h1>
+
         <h1>Meu App</h1>
+        <h1 class="d-none">Meu App Não Mostrado</h1>
+        <h1 class="d-none d-md-flex">Meu App Não Mostrado em Tela Pequena</h1>
+
         <h2 class="text-h2">Meu App</h2>
         <h3 class="text-h6">Meu App</h3>
         <div class="text-subtitle-1">Subtitle-1</div>
@@ -13,23 +18,13 @@
 
         <div class="text-caption">Caption</div>
 
-        <div class="d-flex ma-10 justify-space-around">
-        <div class="w-25 pa-4 border rounded-lg elevation-3">teste</div>
-
-          <div
-            style="width: 100px; height: 100px"
-            class="border rounded-lg elevation-3"
-          >
-            teste
-          </div>
-
-          <div
-            style="width: 100px; height: 100px"
-            class="border rounded-lg elevation-3"
-          >
-            teste
-          </div>
+        <div class="d-flex flex-column flex-md-row ma-10 justify-space-around">
+          <div class="w-100 pa-4 border rounded-lg elevation-3">teste</div>
+          <div class="w-100 pa-4 border rounded-lg elevation-3">teste</div>
+          <div class="w-100 pa-4 border rounded-lg elevation-3">teste</div>
+          <div class="w-100 pa-4 border rounded-lg elevation-3">teste</div>
         </div>
+
       </v-container>
 
       <router-view />
