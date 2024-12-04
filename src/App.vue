fb512e52f3277c11d17571a65ede83c7c32861cb
omartins-zs <gabrielmatheus209@gmail.com>
2024-12-04 14:29:17 -0300

    Add Sidebar e a Lista de Menu do Dashboard

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
 <template>
   <v-app>
+    <v-navigation-drawer v-model="isDrawerOpen">
+      <v-list>
+        <v-list-subheader>Menu</v-list-subheader>
+        <v-list-item prepend-icon="mdi-home">Home</v-list-item>
+        <v-list-item prepend-icon="mdi-account">Usuários</v-list-item>
+
+        <v-list-group value="Clientes">
+          <template #activator="{ props }">
+            <v-list-item v-bind="props" prepend-icon="mdi-account-circle"
+              >Clientes</v-list-item
+            >
+          </template>
+
+          <v-list-item prepend-icon="mdi-currency-usd">Vendas</v-list-item>
+          <v-list-item prepend-icon="mdi-chart-line">Relatório</v-list-item>
+        </v-list-group>
+      </v-list>
+    </v-navigation-drawer>
     <v-app-bar flat class="border-b">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon
+        @click="isDrawerOpen = !isDrawerOpen"
+      ></v-app-bar-nav-icon>
       <v-app-bar-title>Meu APP</v-app-bar-title>
       <template #append>
         <v-btn icon class="mr-2">
@@ -149,4 +169,7 @@
 
 <script setup>
 //
+import { ref } from "vue";
+
+const isDrawerOpen = ref(false);
 </script>
